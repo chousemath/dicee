6bc959b1ebcd1df18950c0b3408e9a89dd2ff29d
Joseph Sungpil Choi <chousemath@gmail.com>
2018-07-17 23:38:24 +0900

    First

---
+++ dicee/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  dicee
+//
+//  Created by Joseph Sungpil Choi on 17/07/2018.
+//  Copyright © 2018 Joseph Sungpil Choi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var randomDiceIndex1: Int = 0
+    var randomDiceIndex2: Int = 0
+
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    @IBAction func handleRoll(_ sender: UIButton) {
+        randomDiceIndex1 = Int(arc4random_uniform(6))
+        randomDiceIndex2 = Int(arc4random_uniform(6))
+        print("randomDiceIndex1: \(randomDiceIndex1)")
+        print("randomDiceIndex2: \(randomDiceIndex2)")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
